89d2e3663a9d8042f4add69fbb85ef543e9447dc
netmeta <moron686@gmail.com>
2023-01-30 22:27:45 +0300

    решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// Массивы из примера
+string[] array1 = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+
+Console.WriteLine("Введите количество символов: "); // в условии 3 
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Исходный массив - [{string.Join(", ", array1)}]");
